d178d9be8d6a3246a6e8251a796d538f93f4d9ca
ayahia1 <70917692+ayahia1@users.noreply.github.com>
2022-06-15 15:38:35 +0200

    Update benchmarkMixed.py

--- benchmarkMixed.py
+++ benchmarkMixed.py
@@ -26,19 +26,31 @@ def split(ids, train, val, test):
 
     return train, val, test
 
+def mix(ratio, X_PA, X_LG, y_PA, y_LG):
+    cntPA = math.floor(ratio * len(y_PA))
+    X_test = np.concatenate((X_PA[:cntPA], X_LG[cntPA:]), axis = 0)
+    y_test = np.concatenate((y_PA[:cntPA], y_LG[cntPA:]), axis = 0)
+    return X_test, y_test
+
 
 def try_models(trainX_PA, trainY_PA, trainX_LG, trainY_LG, ids_PA, ids_LG, 
-               models, N = 4, scoring = None, scale = False, save_trail = '', save = False):
+               models, N = 4, scoring = None, scale = False, save_trail = ''):
 
     train_PA, val_PA, test_PA = split(ids_PA, 0.75, 0.15, 0.10)
     train_LG, val_LG, test_LG = split(ids_LG, 0.75, 0.15, 0.10)
     
-    X_train, y_train = trainX_LG[train_LG], trainY_LG[train_LG]
-    X_val, y_val = trainX_LG[val_LG], trainY_LG[val_LG]
-
-
+    #Full Training, testing, and cross-validation sets for LG and PA
+    X_train_LG, y_train_LG = trainX_LG[train_LG], trainY_LG[train_LG]
+    X_val_LG, y_val_LG = trainX_LG[val_LG], trainY_LG[val_LG]
     X_test_LG, y_test_LG = trainX_LG[test_LG], trainY_LG[test_LG]
+
+    X_train_PA, y_train_PA = trainX_PA[train_PA], trainY_PA[train_PA]
+    X_val_PA, y_val_PA = trainX_PA[val_PA], trainY_PA[val_PA]
     X_test_PA, y_test_PA = trainX_PA[test_PA], trainY_PA[test_PA]
+
+    
+    PA_train_ratio = config['PA_train_ratio']
+    PA_test_ratio = config['PA_test_ratio']
 
 
     all_runs = []
@@ -47,8 +59,6 @@ def try_models(trainX_PA, trainY_PA, trainX_LG, trainY_LG, ids_PA, ids_LG,
     for name, model in models.items():
 
         model_runs = []
-        PA_ratio = 0.2
-
         for i in range(N):
             # create the model with the corresponding parameters
             trainer = model[0](**model[1])
@@ -59,6 +69,14 @@ def try_models(trainX_PA, trainY_PA, trainX_LG, trainY_LG, ids_PA, ids_LG,
             if not os.path.exists(path):
                 os.makedirs(path)
 
+            #Defining the training set
+            X_train, y_train = mix(PA_train_ratio, X_train_PA, X_train_LG,  y_train_PA, y_train_LG)
+            X_val, y_val = mix(PA_train_ratio, X_val_PA, X_val_LG,  y_val_PA, y_val_LG)
+
+            #Defining the testing dataset
+            X_test, y_test = mix(PA_test_ratio, X_test_PA, X_test_LG, y_test_PA, y_test_LG)
+
+            
             if config['retrain']:
                 trainer.fit(X_train, y_train, X_val, y_val)
             else:
@@ -67,13 +85,6 @@ def try_models(trainX_PA, trainY_PA, trainX_LG, trainY_LG, ids_PA, ids_LG,
             if config['save_models']:
                 trainer.save(path)
                 print(path)
-
-
-            #Creating the new testing dataset
-            cntPA = math.floor(PA_ratio * len(y_test_PA))
-            X_test = np.concatenate((X_test_PA[:cntPA], X_test_LG[cntPA:]), axis = 0)
-            y_test = np.concatenate((y_test_PA[:cntPA], y_test_LG[cntPA:]), axis = 0)
-            PA_ratio += 0.2
             
             #Testing the trained models on this dataset
             score = scoring(y_test, trainer.predict(X_test))
@@ -87,10 +98,10 @@ def try_models(trainX_PA, trainY_PA, trainX_LG, trainY_LG, ids_PA, ids_LG,
         
         model_runs = np.array(model_runs)
         model_scores, model_runtimes = model_runs[:,0], model_runs[:,1]
-        statistics.append([name, model_scores.mean(), model_scores.std(), model_runtimes.mean(), model_runtimes.std()])
+        statistics.append([name, PA_train_ratio, PA_test_ratio, model_scores.mean(), model_scores.std(), model_runtimes.mean(), model_runtimes.std()])
 
-    np.savetxt(config['model_dir'] + '/runs' + save_trail+'.csv', all_runs, fmt='%s', delimiter=',', header='Model,Score,Runtime', comments='')
-    np.savetxt(config['model_dir'] + '/statistics' + save_trail+'.csv', statistics, fmt='%s', delimiter=',', header='Model,Mean_score,Std_score,Mean_runtime,Std_runtime', comments='')
+    np.savetxt(config['model_dir'] + '/runs-' + str(PA_train_ratio) + '-' + str(PA_test_ratio) + save_trail +'.csv', all_runs, fmt='%s', delimiter=',', header='Model,Score,Runtime', comments='')
+    np.savetxt(config['model_dir'] + '/statistics-' + str(PA_train_ratio) + '-' + str(PA_test_ratio) + save_trail+'.csv', statistics, fmt='%s', delimiter=',', header='Model, PA_train_ratio, PA_test_ratio, Mean_score,Std_score,Mean_runtime,Std_runtime', comments='')
 
 def benchmark(trainX_PA, trainY_PA, trainX_LG, trainY_LG):
     np.savetxt(config['model_dir']+'/config.csv', [config['task'], config['dataset'], config['preprocessing']], fmt='%s')
